d598263750f29008461339803447f7dbe7247ccc
italoartista <italo.adler@aluno.uece.br>
2024-06-25 21:05:06 -0300

    A solution to the autocomplete feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-// import ListaNomes from './componentes/ListaNomes'
-// import Saudacao from './componentes/Saudacao'
-// import Layout from './componentes/layout/Layout'
-// import Shelf from './componentes/ecommerce/Shelf'
+
 import Search from './componentes/ecommerce/Search'
 
-function Contador({ onAtualizaContador, contador }) {
-
-
-  return (
-    <>
-      <h1>{contador}</h1>
-      <button onClick={() => onAtualizaContador(contador + 1)}>+</button>
-      <button onClick={() => onAtualizaContador(contador - 1)}>-</button>  
-    </>
-  )
-}
 function App() {
   
-  // const produtos = [
-  //   { id: 1, nome: 'Camisa', preco: 50.9999 },
-  //   { id: 2, nome: 'Calça', preco: 100.213801238 },
-  //   { id: 3, nome: 'Tênis', preco: 200.2389403892 }
-  // ]
+ 
   const sugestoes = ['Camisa', 
           'Calça', 
           'Tênis', 
@@ -60,7 +42,7 @@ function App() {
 
     <>
       <Search sugestions={sugestoes}/>
-      {/* <Shelf produtos={produtos}  nome='Produtos mais vendidos' /> */}
+
     </>
 
 
--- src/componentes/ecommerce/Search.jsx
+++ src/componentes/ecommerce/Search.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 
 function Search(  { sugestions }) {
     const [search, setSearch] = useState('')
+    const [suggestions, setSuggestions] = useState(sugestions)
     const [filteredSugestions, setFilteredSugestions] = useState([])
+
+
+    function handleSearch(e) {
+        setSearch(e.target.value)
+        setFilteredSugestions(suggestions.filter(sugestion => sugestion.includes(search)))
+    }
     return (
         <>
-            <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Buscar...' />
+            <input type='text' value={search} onChange={(e)=> handleSearch(e)} placeholder='Buscar...' />
             <p>Pruduto: {search}</p>
-
             <ul>
                 {
-                    filteredSugestions.map((sugestion, index) => <li key={index}>{sugestion}</li>)
+                    filteredSugestions.map((sugestion) => <li key={sugestion}>{sugestion}</li>) 
                 }
-                </ul>
+            </ul>
         </>
     )
 }
